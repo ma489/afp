5e874672aac36a1844f16993d7c074f8846674fe
Mansour <mansour@mansourahmed.com>
2016-07-08 23:59:36 +0200

    adding in-order labelling to tree exercise

--- src/TreeLabeling.hs
+++ src/TreeLabeling.hs
@@ -1,10 +1,13 @@
 -- Tree labeling question: Re-label tree node values so that when reading node values row by row, you get numbers 1,2,3...
+-- i.e. breadth-first labelling 1,2,3...
+-- we don't care about care about the input type
 
 {-# LANGUAGE DeriveFunctor #-}
 
 module TreeLabeling where
 
 import Data.List
+import Control.Monad.State
 
 data Tree a = Leaf | BinTree (Tree a) a (Tree a) deriving (Show, Eq, Functor)
 
@@ -26,3 +29,22 @@ treeInfo (BinTree leftTree _ rightTree) key depth =
 
 sortTreeInfo :: [(String, Int)] -> [(String, Int)]
 sortTreeInfo = sortBy (\(_, d1) (_, d2) -> if d1 > d2 then GT else if d1 < d2 then LT else EQ)
+
+-- extension: (for comparison) in-order, depth-first labelling
+
+labelTreeInOrder :: Tree a -> State Int (Tree Int)
+labelTreeInOrder Leaf = return Leaf
+labelTreeInOrder (BinTree lt v rt) = do
+                                        left <- (labelTreeInOrder lt)
+                                        n <- get
+                                        put (n+1)
+                                        right <- (labelTreeInOrder rt)
+                                        return (BinTree left n right)
+
+t1 = BinTree (BinTree (BinTree Leaf (-3) Leaf) (-2) Leaf) (-1) (BinTree Leaf (-4) Leaf)
+t2 = BinTree
+        (BinTree (BinTree Leaf (-3) Leaf) (-2) Leaf)
+        (-1)
+        (BinTree (BinTree (BinTree Leaf (-3) Leaf) (-2) Leaf) (-1) (BinTree Leaf (-4) Leaf))
+
+doLabeling tree = fst $ runState (labelTreeInOrder tree) 1 --try with t1, or t2
