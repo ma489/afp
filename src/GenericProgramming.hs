4bcef21661651603835098bb5ff22caf9283dbac
Mansour <mansour@mansourahmed.com>
2016-07-10 15:34:08 +0200

    tidying up Generics and SmoothPermutations

--- src/GenericProgramming.hs
+++ src/GenericProgramming.hs
@@ -1,31 +1,57 @@
--- Generic programming (WIP)
 
-{-# LANGUAGE DeriveDataTypeable #-}
+{- Generics in Haskell (datatype-generic programming) -}
 
-import Data.Generics (Data, gshows)
+{-# LANGUAGE DeriveDataTypeable #-} -- required for SYB
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+
+import Data.Generics (Data, gshows) -- SYB
+import GHC.Generics -- this and the below are used by Generic Deriving
+import qualified Generics.Deriving.Show as GenDer
+
+-- custom data type
+data MyType = ThisGuy { someField ::  Bool }
+                -- deriving Show  [want to avoid using the 'default' Show class]
+                deriving (Data,   -- required for SYB
+                          Generic,
+                          GenDer.GShow) -- required for generic-deriving
+
+-- some value to use in examples
+x :: MyType
+x = ThisGuy True
+
+{- 1 - manual approach
+    Defining my own type class for show-able things.
+    It would be tedious to do so for all types -}
 
 class ShowG a where
-    showG :: a -> String
+    showMyGeneric :: a -> String
+
+instance ShowG MyType where
+    showMyGeneric (ThisGuy innerBool) = "ThisGuy (" ++ showMyGeneric innerBool ++ ")"
 
 instance ShowG Bool where
-    showG True = "True"
-    showG False = "False"
+    showMyGeneric True = "True"
+    showMyGeneric False = "False"
 
--- ...
+{- 2 - using SYB
+    simply derive Data -}
 
---Example usage: showG True
+showSYB :: Data a => a -> String
+showSYB thingToShow = gshows thingToShow ""
 
-class ReadG a where
-    readG :: String -> a
+{- 3 - Using generic-deriving -}
 
-instance ReadG Bool where
-    readG "True" = True
-    readG "False" = False
-    readG _ = error "Not a Bool"
+showGenericDeriving :: (GenDer.GShow a) => a -> String
+showGenericDeriving thingToShow = GenDer.gshow thingToShow
 
--- ...
+{- examples -}
 
---Example usage: readG "True" :: Bool
-
-showG' :: Data d => d -> String
-showG' thingToShow = gshows thingToShow ""
+main :: IO ()
+main = do
+        putStr "1 > Showing using my ShowG typeclass: "
+        putStrLn $ showMyGeneric x
+        putStr "2 > Showing using SYB: "
+        putStrLn $ showSYB x
+        putStr "3 > Showing using Generic Deriving: "
+        putStrLn $ showGenericDeriving x
--- src/SmoothPermutations.hs
+++ src/SmoothPermutations.hs
@@ -11,16 +11,16 @@ split (x:xs) = (x, xs):[(y, x:ys) | (y, ys) <- split xs]
 
 perms :: [t] -> [[t]]
 perms [] = [[]]
-perms xs = [(v:p) | (v, vs) <- split xs, p <- perms vs]
+perms xs = [v:p | (v, vs) <- split xs, p <- perms vs]
 
 smooth :: (Num a, Ord a) => a -> [a] -> Bool
 smooth n (x:y:ys) = abs (y - x) <= n && smooth n (y:ys)
 smooth _ _ = True
 
-smooth_perms :: Int -> [Int] -> [[Int]]
-smooth_perms n xs = filter (smooth n) (perms xs)
+smoothPerms :: Int -> [Int] -> [[Int]]
+smoothPerms n xs = filter (smooth n) (perms xs)
 
-{- better (? or equivalent, really?) -}
+{- better -}
 
 -- a tree where each route from root to leaf corresponds to a permutation
 data PermutationTree = Node Int [PermutationTree] deriving Show
@@ -34,22 +34,30 @@ listToPermutationTree' xs = [ Node x (listToPermutationTree' (xs \\ [x])) | x <-
 
 -- generate all permutations from tree
 generatePermutations :: Int -> PermutationTree -> [[Int]]
-generatePermutations n (Node _ startingPoints) = concat $ map (generatePermutations' n) startingPoints
+generatePermutations n (Node _ startingPoints) = concatMap (generatePermutations' n) startingPoints
 
 generatePermutations' :: Int -> PermutationTree -> [[Int]] -- list of sequences starting with v
-generatePermutations' n (Node v []) = [[v]]
-generatePermutations' n (Node v children) = [ (v:p) | p <- concat $ map (generatePermutations' n) $ filter (\(Node v2 _) -> abs (v - v2) <= n) children ]
+generatePermutations' _ (Node v []) = [[v]]
+generatePermutations' n (Node v children) = [ v:p | p <- concatMap (generatePermutations' n) $ filter (smoothBranch v) children ]
+                                                where smoothBranch v1 (Node v2 _) = abs (v1 - v2) <= n
 
-smooth_perms_improved :: Int -> [Int] -> [[Int]]
-smooth_perms_improved n xs = generatePermutations n $ listToPermutationTree xs
+smoothPermsImproved :: Int -> [Int] -> [[Int]]
+smoothPermsImproved n xs = generatePermutations n $ listToPermutationTree xs
 
 {- examples -}
+
 --naive
-example1Naive = smooth_perms 1 [1,2] -- expect: [[1,2],[2,1]]
-example2Naive = smooth_perms 1 [1,2,3] -- expect: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
-example3Naive = smooth_perms 1 [1,2,3,4,5] -- expect: [[1,2,3,4,5],[5,4,3,2,1]]
+example1Naive :: [[Int]]
+example1Naive = smoothPerms 1 [1,2] -- expect: [[1,2],[2,1]]
+example2Naive :: [[Int]]
+example2Naive = smoothPerms 1 [1,2,3] -- expect: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+example3Naive :: [[Int]]
+example3Naive = smoothPerms 1 [1,2,3,4,5] -- expect: [[1,2,3,4,5],[5,4,3,2,1]]
 
 --better
-example1Better = smooth_perms_improved 1 [1,2] -- expect: [[1,2],[2,1]]
-example2Better = smooth_perms_improved 1 [1,2,3] -- expect: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
-example3Better = smooth_perms_improved 1 [1,2,3,4,5] -- expect: [[1,2,3,4,5],[5,4,3,2,1]]
+example1Better :: [[Int]]
+example1Better = smoothPermsImproved 1 [1,2] -- expect: [[1,2],[2,1]]
+example2Better :: [[Int]]
+example2Better = smoothPermsImproved 1 [1,2,3] -- expect: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+example3Better :: [[Int]]
+example3Better = smoothPermsImproved 1 [1,2,3,4,5] -- expect: [[1,2,3,4,5],[5,4,3,2,1]]
